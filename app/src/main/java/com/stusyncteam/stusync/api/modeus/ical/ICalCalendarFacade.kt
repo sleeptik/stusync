f031de7b6c3248040fa8b17e78cfd8f454251dff
sleeptik <66788142+sleeptik@users.noreply.github.com>
2023-04-19 16:06:22 +0500

    add fix for ICalCalendarFacade for ics with timezone badly specified

--- app/src/main/java/com/stusyncteam/stusync/api/modeus/ical/ICalCalendarFacade.kt
+++ app/src/main/java/com/stusyncteam/stusync/api/modeus/ical/ICalCalendarFacade.kt
@@ -5,11 +5,25 @@ import net.fortuna.ical4j.data.CalendarBuilder
 import net.fortuna.ical4j.model.Calendar
 import net.fortuna.ical4j.model.component.VEvent
 import java.io.InputStream
+import java.io.StringReader
 
 class ICalCalendarFacade private constructor(private val calendar: Calendar) {
     companion object {
         fun fromStream(stream: InputStream): ICalCalendarFacade {
-            return ICalCalendarFacade(CalendarBuilder().build(stream))
+            val formattedStream = formatTimeStampsAndGetStream(stream)
+            formattedStream.use {
+                return ICalCalendarFacade(CalendarBuilder().build(it))
+            }
+        }
+
+        private fun formatTimeStampsAndGetStream(stream: InputStream): StringReader {
+            // TODO: Replace with regex to support different timezones
+            val toReplace = ";TZID=Asia/Yekaterinburg"
+            val replaceWith = ""
+            return stream.bufferedReader()
+                .readText()
+                .replace(toReplace, replaceWith)
+                .reader()
         }
     }
 
