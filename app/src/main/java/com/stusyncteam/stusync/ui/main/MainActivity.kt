0a1c1875c8d70c17d5101e553d67235a4dd92f00
sleeptik <66788142+sleeptik@users.noreply.github.com>
2023-06-04 22:00:00 +0500

    clean code in MainActivity

--- app/src/main/java/com/stusyncteam/stusync/ui/main/MainActivity.kt
+++ app/src/main/java/com/stusyncteam/stusync/ui/main/MainActivity.kt
@@ -1,19 +1,14 @@
-@file:Suppress("JoinDeclarationAndAssignment")
-
 package com.stusyncteam.stusync.ui.main
 
-import android.Manifest.permission.*
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
+import android.widget.ImageButton
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.stusyncteam.modeus.ModeusSession
 import com.stusyncteam.modeus.api.auth.ModeusSignIn
@@ -21,7 +16,6 @@ import com.stusyncteam.stusync.R
 import com.stusyncteam.stusync.api.google.GoogleCalendarFacade
 import com.stusyncteam.stusync.background.AutoSyncWorkScheduler
 import com.stusyncteam.stusync.databinding.ActivityMainBinding
-import com.stusyncteam.stusync.permissions.PermissionRequester
 import com.stusyncteam.stusync.storage.credentials.CredentialsStorage
 import com.stusyncteam.stusync.storage.settings.AutoSyncSettings
 import com.stusyncteam.stusync.storage.settings.AutoSyncSettingsStorage
@@ -35,105 +29,93 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private val syncStatsViewModel = SyncStatsViewModel()
 
-    private var consentLauncher: ActivityResultLauncher<Intent>
-    private var openSettingsLauncher: ActivityResultLauncher<Intent>
+    private val openSettingLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { }
 
-    private val autoSyncSettingsStorage = AutoSyncSettingsStorage(this)
-    private val syncViewModel = SyncStatsViewModel()
+    private val modeusSession by lazy { runBlocking { getNewModeusSession() } }
 
-    private lateinit var modeusSession: ModeusSession
-
-    init {
-        consentLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) {
-            if (it.resultCode != RESULT_OK) {
-                Toast.makeText(this, "Consent was not given", Toast.LENGTH_LONG)
-                    .show()
-            }
+    private val askConsentLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+    }
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        if (throwable is UserRecoverableAuthIOException) {
+            askConsentLauncher.launch(throwable.intent)
         }
-
-        openSettingsLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
 
-        runBlocking {
-            val syncStats = SyncStatsStorage(this@MainActivity).load() ?: SyncStats()
-            syncViewModel.syncStats.value = syncStats
-            binding.stats = syncViewModel
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).also {
+            lifecycleScope.launch {
+                refreshSyncStats()
+                it.stats = syncStatsViewModel
+            }
+            it.lifecycleOwner = this
         }
 
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                val credentials = CredentialsStorage(this@MainActivity).load()
-                modeusSession = ModeusSignIn.login(credentials!!)
+        findViewById<ImageButton>(R.id.btn_open_settings).also {
+            it.setOnClickListener {
+                val intent = Intent(this, SettingsActivity::class.java)
+                openSettingLauncher.launch(intent)
             }
         }
 
-        findViewById<FloatingActionButton>(R.id.btn_open_settings).setOnClickListener {
-            openSettingsLauncher.launch(Intent(this, SettingsActivity::class.java))
-        }
+        findViewById<Button>(R.id.btn_manual_sync).also {
+            it.setOnClickListener {
+                lifecycleScope.launch(coroutineExceptionHandler) {
+                    it.isEnabled = false
 
-        findViewById<Button>(R.id.btn_manual_sync).setOnClickListener {
-            PermissionRequester.requestGetAccountsPermission(this)
+                    withContext(Dispatchers.IO) {
+                        val person = modeusSession.getMyself()
+                        val events = modeusSession.getPersonEvents(person)
 
-            it.isEnabled = false
-            lifecycleScope.launch(handleRequestExecutionWithAuth())
-            {
-                withContext(Dispatchers.IO) {
-                    val googleCalendar = GoogleCalendarFacade.fromContext(this@MainActivity)
+                        val calendar = GoogleCalendarFacade.fromContext(this@MainActivity)
+                        calendar.updateCalendar(events)
+                    }
 
-                    val self = modeusSession.getMyself()
-                    val events = modeusSession.getPersonEvents(self).toMutableList()
+                    refreshSyncStats()
 
-                    googleCalendar.updateCalendar(events)
+                    it.isEnabled = true
                 }
-                val syncStats = SyncStatsStorage(this@MainActivity).load() ?: SyncStats()
-                syncViewModel.syncStats.value = syncStats
-
-                it.isEnabled = true
             }
         }
 
-        val autoSyncSwitch = findViewById<SwitchCompat>(R.id.sw_auto_sync)
-        autoSyncSwitch.isChecked = loadAutoSyncSettings().isEnabled
-        autoSyncSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                AutoSyncWorkScheduler.scheduleAutoSync(this)
-            } else {
-                AutoSyncWorkScheduler.disableAutoSync(this)
-            }
-
-            val autoSyncSettings = loadAutoSyncSettings().also { it.isEnabled = isChecked }
-            saveAutoSyncSettings(autoSyncSettings)
-        }
-    }
-
-    private fun saveAutoSyncSettings(autoSyncSettings: AutoSyncSettings) {
-        runBlocking {
-            autoSyncSettingsStorage.save(autoSyncSettings)
-        }
-    }
-
-    private fun loadAutoSyncSettings(): AutoSyncSettings {
-        return runBlocking {
-            autoSyncSettingsStorage.load() ?: AutoSyncSettings()
-        }
-    }
-
-    private fun handleRequestExecutionWithAuth(): CoroutineExceptionHandler {
-        return CoroutineExceptionHandler { _, throwable ->
-            if (throwable is UserRecoverableAuthIOException) {
-                consentLauncher.launch(throwable.intent)
+        findViewById<SwitchCompat>(R.id.sw_auto_sync).also {
+            lifecycleScope.launch { it.isChecked = getAutoSyncState() }
+            it.setOnCheckedChangeListener { _, isChecked ->
+                lifecycleScope.launch {
+                    when (isChecked) {
+                        true -> AutoSyncWorkScheduler.scheduleAutoSync(this@MainActivity)
+                        false -> AutoSyncWorkScheduler.disableAutoSync(this@MainActivity)
+                    }
+                    setAutoSyncState(isChecked)
+                }
             }
         }
+    }
+
+    private suspend fun getNewModeusSession(): ModeusSession = withContext(Dispatchers.IO) {
+        val credentials = CredentialsStorage(this@MainActivity).load()!!
+        return@withContext ModeusSignIn.login(credentials)
+    }
+
+    private suspend fun refreshSyncStats() {
+        syncStatsViewModel.syncStats.value = SyncStatsStorage(this).load() ?: SyncStats()
+    }
+
+    private suspend fun getAutoSyncState(): Boolean {
+        val settings = AutoSyncSettingsStorage(this).load() ?: AutoSyncSettings()
+        return settings.isEnabled
+    }
+
+    private suspend fun setAutoSyncState(isEnabled: Boolean) {
+        val storage = AutoSyncSettingsStorage(this)
+        val settings = storage.load() ?: AutoSyncSettings()
+        settings.isEnabled = isEnabled
+        storage.save(settings)
     }
 }
