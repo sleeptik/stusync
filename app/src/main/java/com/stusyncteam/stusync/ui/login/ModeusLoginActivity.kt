f074c403313652eb5ec82b12a7af16e9021ea0fa
sleeptik <66788142+sleeptik@users.noreply.github.com>
2023-06-04 22:52:27 +0500

    clean code in ModeusLoginActivity

--- app/src/main/java/com/stusyncteam/stusync/ui/login/ModeusLoginActivity.kt
+++ app/src/main/java/com/stusyncteam/stusync/ui/login/ModeusLoginActivity.kt
@@ -6,62 +6,63 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.stusyncteam.modeus.api.auth.ModeusSignIn
 import com.stusyncteam.modeus.api.auth.UserCredentials
 import com.stusyncteam.stusync.R
 import com.stusyncteam.stusync.storage.credentials.CredentialsStorage
 import com.stusyncteam.stusync.ui.main.MainActivity
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class ModeusLoginActivity : AppCompatActivity(), OnLoginFinished {
-    private val storage: CredentialsStorage by lazy { CredentialsStorage(this) }
+    private val credentialsStorage: CredentialsStorage by lazy { CredentialsStorage(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modeus_login)
 
-        if (loadCredentials() != null)
-            onLoginFinished()
+        runBlocking {
+            val result = kotlin.runCatching { loadCredentials() }
+
+            if (result.isSuccess)
+                onLoginFinished()
+        }
 
         findViewById<Button>(R.id.btn_sign_in_modeus).setOnClickListener {
-            val login = findViewById<EditText>(R.id.et_login).text.toString()
-            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            lifecycleScope.launch {
+                it.isEnabled = false
 
-            if (login == "" || password == "")
-                return@setOnClickListener
+                val login = findViewById<EditText>(R.id.et_login).text.toString()
+                val password = findViewById<EditText>(R.id.et_password).text.toString()
 
-            val userCredentials = UserCredentials(login, password)
+                if (login == "" || password == "")
+                    return@launch
 
-            try {
-                runBlocking {
-                    withContext(Dispatchers.IO) {
+                val userCredentials = UserCredentials(login, password)
+
+                withContext(Dispatchers.IO) {
+                    try {
                         ModeusSignIn.login(userCredentials)
+                        saveCredentials(userCredentials)
+                        onLoginFinished()
+                    } catch (e: Exception) {
+                        Log.e("login error", e.message.toString())
+                        it.isEnabled = true
                     }
                 }
-                saveCredentials(userCredentials)
-                onLoginFinished()
-            } catch (e: Exception) {
-                Log.e("login error", e.message.toString())
             }
         }
     }
 
-    private fun saveCredentials(userCredentials: UserCredentials) {
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                storage.save(userCredentials)
-            }
-        }
+    private suspend fun saveCredentials(userCredentials: UserCredentials) {
+        credentialsStorage.save(userCredentials)
     }
 
-    private fun loadCredentials(): UserCredentials {
-        return runBlocking {
-            withContext(Dispatchers.IO) {
-                storage.load()
-            }
-        }
+    private suspend fun loadCredentials(): UserCredentials {
+        return credentialsStorage.load()
     }
 
     override fun onLoginFinished() {
